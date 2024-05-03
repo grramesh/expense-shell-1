36c4f320a24fc7a90c2e33570ee9b1f40ea6abb4
Ramesh <ramgr498@gmail.com>
2024-05-03 16:29:01 +0530

    source code

--- backend.sh
+++ backend.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log #It will log in tmp directory as .log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-B="\e[34m"
+check_root  # to call check_root from other script, it has to be in function
+
 echo "please enter db password:"
 read -s mysql_root_password
 
-VALIDATE(){    
-       if [ $1 -ne 0 ]
-      then
-         echo -e "$2..$R FAILURE $N"
-         exit 1
-      else
-         echo -e "$2.. $G SUCCESS $N"
-    fi 
-}
-
-if [ $USERID -ne 0 ]
-then 
-    echo "please run this script in root access"
-    exit 1 # manually exit if error come.# 
-else
-echo "you are root user." 
-
-fi
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $?"disable default nodejs"
 dnf module enable nodejs:20 -y &>>$LOGFILE
--- frontend.sh
+++ frontend.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log #It will log in tmp directory as .log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-B="\e[34m"
-
-VALIDATE(){    
-       if [ $1 -ne 0 ]
-      then
-         echo -e "$2..$R FAILURE $N"
-         exit 1
-      else
-         echo -e "$2.. $G SUCCESS $N"
-    fi 
-}
-
-if [ $USERID -ne 0 ]
-then 
-    echo "please run this script in root access"
-    exit 1 # manually exit if error come.# 
-else
-echo "you are root user." 
-
-fi
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
